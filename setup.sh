6e899b33a9a61945d56ff55f124789f60aa8eebb
Ruslan Korolev <great1672@gmail.com>
2020-01-28 10:00:29 +0000

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 set -e
 
+# TODO: rewrite this script without mv and git clone in root
+
 echo 'Setup script - Run it under root'
 
-source add-user-sh
+source add-user.sh
 source setup-docker.sh
 
 mv /root/bash_scripts_webpagetest /home/deploy/
 
 echo 'OK, next - add ssh key for deploy user'
 echo 'And on deploy user run setup-location.sh, start-location.sh from deploy user'
-# TODO: login on deploy and run scripts
+
+echo 'Setup locaton script - Running under deploy user'
+su - deploy
+source setup-location.sh
+echo 'Finish: Add credentials to credentials.sh and run ./start-location.sh under deploy user'
+
