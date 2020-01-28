4e1de798425d3d70dc581041a62597bdba070f66
Ruslan Korolev <great1672@gmail.com>
2020-01-28 12:46:05 +0000

    automatically restart docker on startup

--- start-location.sh
+++ start-location.sh
@@ -7,7 +7,7 @@ echo 'Skip kill all docker containers'
 #docker kill $(docker ps -q)
 
 echo 'Run webpagetest location instance'
-docker run -d -p 4001:80 \
+docker run --restart=unless-stopped -d -p 4001:80 \
 	   --network="host" \
 	      -e "SERVER_URL=$SERVER_URL" \
 	         -e "LOCATION=$LOCATION_UID" \
