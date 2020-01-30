741b56a50b0fbbb83ff2841a7da4904310455732
Ruslan Korolev <great1672@gmail.com>
2020-01-30 09:46:40 +0200

    change scripts for full setup location from scratch

--- setup-location.sh
+++ setup-location.sh
@@ -8,7 +8,6 @@ echo 'build customized agent from ./agent folder'
 cd agent && (docker build --no-cache -t webpagetest_agent .)
 
 docker image rm webpagetest/agent
-docker image rm hello-world
 
 echo 'Copy credentials, please see credentials.sh'
 cp credentials-sample.sh credentials.sh
