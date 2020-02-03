f6818ea673f0d76a3435a85520ba0633361fd6a7
Ruslan Korolev <great1672@gmail.com>
2020-02-03 15:30:34 +0200

    update setup-cron.sh

---
+++ setup-cron.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+set -e
+servers=() # ip addresses of servers
+
+for i in "${servers[@]}"
+do
+   echo $i
+   ssh -t deploy@$i 'cd /home/deploy/bash_scripts_webpagetest && git checkout master && git pull; cd cron; ./update-cron.sh'
+done
--- setup.sh
+++ setup.sh
@@ -2,10 +2,14 @@
 
 set -e
 SERVER_URL=''
+WPT_SERVER=''
+WPT_UID=''
 
+#
 echo 'add root ssh key'
 ssh-copy-id -i ~/.ssh/github_rsa.pub root@$SERVER_URL -o PreferredAuthentications=password -o PubkeyAuthentication=no
 
+#
 echo 'add deploy user'
 ssh -t root@$SERVER_URL 'adduser deploy;sudo groupadd docker;sudo usermod -aG docker deploy;'
 
@@ -18,7 +22,17 @@ ssh -t deploy@$SERVER_URL 'cd /home/deploy && git clone https://github.com/rusik
 echo 'setup docker under root'
 ssh -t root@$SERVER_URL '(cd /home/deploy/bash_scripts_webpagetest && ./setup-docker.sh)'
 
-echo 'setup location under deploy user'
-ssh -t deploy@$SERVER_URL '(cd /home/deploy/bash_scripts_webpagetest && ./setup-location.sh)'
+echo 'Setup location under deploy user , create credentials, start docker'
+ssh -t deploy@$SERVER_URL<< HERE
+  cd /home/deploy/bash_scripts_webpagetest
+  echo 'Setup location'
+  ./setup-location.sh
+  echo 'create credentials'
+  touch credentials.sh
+  echo 'SERVER_URL=$WPT_SERVER' >> credentials.sh
+  echo 'LOCATION_UID=$WPT_UID' >> credentials.sh
+  echo 'start location'
+  ./start-location.sh
+HERE
 
-echo 'Finish! Please add/change credentials.sh from credentials-sample.sh and run ./start-location.sh and add optional: add to cron for autorestart location'
+echo 'Finish!, Please add cron/restart.sh to cron under deploy user to prevent memory issues'
