c3e16690c6afe40e77c46a7d8f0e5ee87357bfa2
Ruslan Korolev <great1672@gmail.com>
2020-07-14 16:30:54 +0300

    pass ENV outside bash script

--- setup.sh
+++ setup.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 set -e
-SERVER_URL=''
-WPT_SERVER=''
-WPT_UID=''
+# Setup env variables: SERVER_URL, WPT_SERVER, WPT_UID
 
+if [ -z "$SERVER_URL" ] || [ -z "$WPT_SERVER" ] || [ -z "$WPT_UID" ]
+then
+  echo "Error: ENV SERVER_URL or WPT_SERVER or WPT is empty"
+  exit
+fi
 
 echo 'add root ssh key'
 ssh-copy-id -i ~/.ssh/github_rsa.pub root@$SERVER_URL -o PreferredAuthentications=password -o PubkeyAuthentication=no
 
-#
 echo 'add deploy user'
-ssh -t root@$SERVER_URL 'adduser deploy;sudo groupadd docker;sudo usermod -aG docker deploy;'
+ssh -t root@$SERVER_URL 'adduser deploy;sudo groupadd docker;sudo usermod -aG docker deploy;sudo apt-get install git;'
 
 echo 'add deploy ssh key'
 ssh-copy-id -i ~/.ssh/github_rsa.pub deploy@$SERVER_URL -o PreferredAuthentications=password -o PubkeyAuthentication=no
